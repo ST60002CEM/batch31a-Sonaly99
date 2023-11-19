70423480514b6631bb2b1f0ebacadd01c1a54302
sonal <sonaly955@gmail.com>
2023-11-19 12:49:44 +0545

    updated

---
+++ lib/Screens/controller/splash_screen_controller.dart
@@ -0,0 +1,17 @@
+import 'package:furpixs/Screens/welcomescreen.dart';
+import 'package:get/get.dart';
+
+class SplashScreenController extends GetxController{
+  static SplashScreenController get find => Get.find();
+  RxBool animate =  false.obs;
+
+  Future startAnimation()async {
+    await Future.delayed( const Duration(milliseconds: 500));
+    animate.value = true;
+    await Future.delayed(const Duration(milliseconds: 500));
+    Get.to((WelcomeScreen()));
+/*
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Welcomescreen));
+*/
+  }
+}
--- lib/Screens/splash_screen.dart
+++ lib/Screens/splash_screen.dart
@@ -1,96 +1,96 @@
+import 'dart:ffi';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:furpixs/Screens/controller/splash_screen_controller.dart';
 import 'package:furpixs/constants/image_strings.dart';
 import 'package:furpixs/constants/sizes.dart';
 import 'package:furpixs/constants/text_strings.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 
 import '../constants/colors.dart';
 
-class SplashScreen extends StatefulWidget{
-  const SplashScreen({Key? key}) : super(key: key);
-  @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
+class SplashScreen extends StatelessWidget {
+  SplashScreen({Key? key}) : super(key: key);
 
-class _SplashScreenState extends State<SplashScreen> {
+  final splashController = Get.put(SplashScreenController());
 
-  bool animate =  false;
-  @override
-  void initState(){
-    startAnimation();
-
-
-  }
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    splashController.startAnimation();
+    return Scaffold(
       body: Stack(
         children: [
-           AnimatedPositioned(
-            top: animate ? 0 : -30,
-              left: animate ? 0 : -30,
+          Obx(
+          () => AnimatedPositioned(
+              top: splashController.animate.value ? 0 : -30,
+              left: splashController.animate.value ? 0 : -30,
               duration: const Duration(milliseconds: 1),
               child: const Image(
                 image: AssetImage(tSplashTopIcon),
               ),
+            ),
           ),
-          AnimatedPositioned(
-            duration: const Duration(milliseconds: 1600),
-           top: 80,
-              left: animate ?  tDefaultSize : -80,
+          Obx( () =>
+            AnimatedPositioned(
+              duration: const Duration(milliseconds: 1600),
+              top: 80,
+              left: splashController.animate.value ? tDefaultSize : -80,
               child: AnimatedOpacity(
-                opacity: animate ? 1: 0,
+                opacity: splashController.animate.value ? 1 : 0,
                 duration: const Duration(milliseconds: 1600),
                 child: Column(
-                  crossAxisAlignment:  CrossAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(tAppName, style: Theme.of(context).textTheme.headline3,),
-                    Text(tAppTagLine, style: Theme.of(context).textTheme.headline2,)
+                    Text(tAppName, style: Theme
+                        .of(context)
+                        .textTheme
+                        .headline3,),
+                    Text(tAppTagLine, style: Theme
+                        .of(context)
+                        .textTheme
+                        .headline2,)
                   ],
                 ),
               ),
+            ),
           ),
 
-          AnimatedPositioned( duration: const Duration(milliseconds: 2400),
-            bottom: animate ? 100 : 0,
-            child: AnimatedOpacity(
-              duration: const Duration(milliseconds: 2000),
-              opacity: animate ? 1 : 0,
-              child: const Image(image : AssetImage(tSplashImage)),
+          Obx(() =>
+            AnimatedPositioned(duration: const Duration(milliseconds: 2400),
+                bottom: splashController.animate.value ? 100 : 0,
+                child: AnimatedOpacity(
+                  duration: const Duration(milliseconds: 2000),
+                  opacity: splashController.animate.value ? 1 : 0,
+                  child: const Image(image: AssetImage(tSplashImage)),
 
-            )),
-          AnimatedPositioned(
-            bottom: animate ? 60 : 0,
+                )),
+          ),
+          Obx(() => AnimatedPositioned(
+              bottom: splashController.animate.value ? 60 : 0,
               right: tDefaultSize,
-            duration: const Duration(milliseconds: 2400),
-            child: AnimatedOpacity(
-              duration: const Duration(milliseconds: 2000),
-              opacity: animate ? 1 : 0,
-              child: Container(
-                width: tSplashContainerSize,
-                height: tSplashContainerSize,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(100),
-                  color: tPrimaryColor,
+              duration: const Duration(milliseconds: 2400),
+              child: AnimatedOpacity(
+                duration: const Duration(milliseconds: 2000),
+                opacity: splashController.animate.value ? 1 : 0,
+                child: Container(
+                  width: tSplashContainerSize,
+                  height: tSplashContainerSize,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                    color: tPrimaryColor,
+                  ),
                 ),
               ),
-            ),
 
+            ),
           ),
 
 
         ],
       ),
-      );
+    );
+  }
+}
 
-  }
-
-  Future startAnimation()async {
-    await Future.delayed( const Duration(milliseconds: 500));
-    setState(() => animate = true);
-    await Future.delayed(const Duration(milliseconds: 500));
-/*
-    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Welcomescreen));
-*/
-  }
-  }
