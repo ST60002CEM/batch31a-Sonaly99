f9265ce8167e5cc3207c1f8a8065989d8deee690
sonal <sonaly955@gmail.com>
2023-11-19 11:59:22 +0545

    updated

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,2 +1,9 @@
 import 'dart:ui';
 
+const tPrimaryColor =  Color(0xFFD1C4E9);
+const tSecondaryColor  = Color(0xFF272727);
+const tAccentColor =  Color(0xFF673AB7);
+
+const tCardBgColor =  Color(0xFFF7F6F1);
+const tWhiteColor =  Color(0xffffffff);
+const tDarkColor  = Color(0xFF000000);
